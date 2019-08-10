169e7922045ce5200dcf0b08f7986c166d5ac1d6
Andrei Churyla <AndrewChuriloMinsk@gmail.com>
2019-08-10 19:36:14 +0300

    RulesActivity: avoid memory leak

--- app/src/main/java/com/morrowbone/mafiacards/app/activity/RulesActivity.java
+++ app/src/main/java/com/morrowbone/mafiacards/app/activity/RulesActivity.java
@@ -2,17 +2,18 @@ package com.morrowbone.mafiacards.app.activity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.fragment.app.FragmentActivity;
 
 import com.morrowbone.mafiacards.app.R;
 import com.morrowbone.mafiacards.app.adapter.RolesArrayAdapter;
+import com.morrowbone.mafiacards.app.application.MafiaApp;
 import com.morrowbone.mafiacards.app.data.AbstractCard;
 import com.morrowbone.mafiacards.app.data.CardRepository;
 import com.morrowbone.mafiacards.app.utils.InjectorUtils;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 public class RulesActivity extends FragmentActivity {
@@ -22,24 +23,31 @@ public class RulesActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
 
-        init();
+        ListView listView = (ListView) findViewById(R.id.game_type_listview);
+        LoadRolesAsyncTask rolesAsyncTask = new LoadRolesAsyncTask(listView);
+        rolesAsyncTask.execute();
     }
 
-    private void init() {
-        AsyncTask<Void, Void, List<AbstractCard>> asyncTask = new AsyncTask<Void, Void, List<AbstractCard>>() {
-            @Override
-            protected List<AbstractCard> doInBackground(Void... voids) {
-                CardRepository cardRepository = InjectorUtils.INSTANCE.getCardRepository(RulesActivity.this);
-                return cardRepository.getCards();
-            }
+    private static class LoadRolesAsyncTask extends AsyncTask<Void, Void, List<AbstractCard>> {
+        private WeakReference<ListView> weakListView;
 
-            @Override
-            protected void onPostExecute(List<AbstractCard> abstractCards) {
-                RolesArrayAdapter adapter = new RolesArrayAdapter(RulesActivity.this, abstractCards);
-                ListView listView = (ListView) findViewById(R.id.game_type_listview);
+        private LoadRolesAsyncTask(ListView listView) {
+            this.weakListView = new WeakReference<>(listView);
+        }
+
+        @Override
+        protected List<AbstractCard> doInBackground(Void... voids) {
+            CardRepository cardRepository = InjectorUtils.INSTANCE.getCardRepository(MafiaApp.getInstance());
+            return cardRepository.getCards();
+        }
+
+        @Override
+        protected void onPostExecute(List<AbstractCard> abstractCards) {
+            ListView listView = weakListView.get();
+            if (listView != null) {
+                RolesArrayAdapter adapter = new RolesArrayAdapter(MafiaApp.getInstance(), abstractCards);
                 listView.setAdapter(adapter);
             }
-        };
-        asyncTask.execute();
+        }
     }
 }
