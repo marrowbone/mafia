da2af9b6453c9bdf4b2d0a2b9f01f4abaaaceecf
Andrei Churyla <AndrewChuriloMinsk@gmail.com>
2019-08-16 01:01:51 +0300

    Update database (don't save resId)
    Closes https://trello.com/c/xPoqrHek/19-refactor-default-cards

--- app/src/main/java/com/morrowbone/mafiacards/app/data/DefaultCard.kt
+++ app/src/main/java/com/morrowbone/mafiacards/app/data/DefaultCard.kt
@@ -3,19 +3,53 @@ package com.morrowbone.mafiacards.app.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.morrowbone.mafiacards.app.R
 import com.morrowbone.mafiacards.app.application.MafiaApp
 
 @Entity(tableName = "default_cards")
 data class DefaultCard(
-        @PrimaryKey @ColumnInfo(name = "id") val cardId: String,
-        val nameResId: Int,
-        val descriptionResId: Int,
-        val pictureResId: Int
+        @PrimaryKey @ColumnInfo(name = "id") val cardId: String
 ) : AbstractCard {
     override fun getId(): String = cardId
-    override fun getTitle(): String = MafiaApp.instance!!.getString(nameResId)
-    override fun getDescription(): String = MafiaApp.instance!!.getString(descriptionResId)
-    override fun getImageResId(): Int = pictureResId
+    override fun getTitle(): String {
+        val nameResId = when (cardId) {
+            CIVILIAN -> R.string.role_civilian
+            MAFIA -> R.string.role_mafia
+            DETECTIVE -> R.string.role_detective
+            DON_MAFIA -> R.string.role_don
+            DOCTOR -> R.string.role_doctor
+            IMMORTAL -> R.string.role_immortal
+            MANIAC -> R.string.role_maniac
+            else -> R.string.role_civilian
+        }
+
+        return MafiaApp.instance!!.getString(nameResId)
+    }
+
+    override fun getDescription(): String {
+        val descriptionResId = when (cardId) {
+            CIVILIAN -> R.string.role_civilian_info
+            MAFIA -> R.string.role_mafia_info
+            DETECTIVE -> R.string.role_detective_info
+            DON_MAFIA -> R.string.role_don_info
+            DOCTOR -> R.string.role_don_info
+            IMMORTAL -> R.string.role_immortal_info
+            MANIAC -> R.string.role_maniac_info
+            else -> R.string.role_civilian_info
+        }
+        return MafiaApp.instance!!.getString(descriptionResId)
+    }
+
+    override fun getImageResId(): Int = when (cardId) {
+        CIVILIAN -> R.drawable.civilian_cr
+        MAFIA -> R.drawable.mafia_cr
+        DETECTIVE -> R.drawable.detective_cr
+        DON_MAFIA -> R.drawable.don_cr
+        DOCTOR -> R.drawable.doctor_cr
+        IMMORTAL -> R.drawable.immortal_cr
+        MANIAC -> R.drawable.manic_cr
+        else -> R.drawable.civilian_cr
+    }
 
     companion object {
         const val CIVILIAN = "civilian"
--- app/src/main/java/com/morrowbone/mafiacards/app/workers/SeedDatabaseWorker.kt
+++ app/src/main/java/com/morrowbone/mafiacards/app/workers/SeedDatabaseWorker.kt
@@ -56,40 +56,12 @@ class SeedDatabaseWorker(
     }
 
     private fun getDefaultCards(): List<DefaultCard> = mutableListOf<DefaultCard>().apply {
-        add(DefaultCard(
-                DefaultCard.CIVILIAN,
-                R.string.role_civilian,
-                R.string.role_civilian_info,
-                R.drawable.civilian_cr))
-        add(DefaultCard(
-                DefaultCard.MAFIA,
-                R.string.role_mafia,
-                R.string.role_mafia_info,
-                R.drawable.mafia_cr))
-        add(DefaultCard(
-                DefaultCard.DETECTIVE,
-                R.string.role_detective,
-                R.string.role_detective_info,
-                R.drawable.detective_cr))
-        add(DefaultCard(
-                DefaultCard.DON_MAFIA,
-                R.string.role_don,
-                R.string.role_don_info,
-                R.drawable.don_cr))
-        add(DefaultCard(
-                DefaultCard.DOCTOR,
-                R.string.role_doctor,
-                R.string.role_doctor_info,
-                R.drawable.doctor_cr))
-        add(DefaultCard(
-                DefaultCard.IMMORTAL,
-                R.string.role_immortal,
-                R.string.role_immortal_info,
-                R.drawable.immortal_cr))
-        add(DefaultCard(
-                DefaultCard.MANIAC,
-                R.string.role_maniac,
-                R.string.role_maniac_info,
-                R.drawable.manic_cr))
+        add(DefaultCard(DefaultCard.CIVILIAN))
+        add(DefaultCard(DefaultCard.MAFIA))
+        add(DefaultCard(DefaultCard.DETECTIVE))
+        add(DefaultCard(DefaultCard.DON_MAFIA))
+        add(DefaultCard(DefaultCard.DOCTOR))
+        add(DefaultCard(DefaultCard.IMMORTAL))
+        add(DefaultCard(DefaultCard.MANIAC))
     }
 }
