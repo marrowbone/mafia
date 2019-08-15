5837a65eee9e3b983cfa7aec192f8b668759ffac
Andrei Churyla <AndrewChuriloMinsk@gmail.com>
2019-08-15 20:30:35 +0300

    Fix accent color
    Closes https://trello.com/c/xCRxq6sS/14-fix-color-of-dialogs-buttons

--- app/src/main/java/com/morrowbone/mafiacards/app/activity/MainActivity.kt
+++ app/src/main/java/com/morrowbone/mafiacards/app/activity/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.morrowbone.mafiacards.app.activity
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import com.morrowbone.mafiacards.app.R
 
-class MainActivity : FragmentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
