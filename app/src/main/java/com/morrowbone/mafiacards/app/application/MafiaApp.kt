f28ac7bbc5f2cc8c6b33b4061b3354dc0ef4ce03
Andrei Churyla <AndrewChuriloMinsk@gmail.com>
2019-08-19 00:43:37 +0300

    Force the database  pre-population
    Closes https://trello.com/c/f5SoJwL9/32-fix-crash-with-db-initialization

--- app/src/main/java/com/morrowbone/mafiacards/app/application/MafiaApp.kt
+++ app/src/main/java/com/morrowbone/mafiacards/app/application/MafiaApp.kt
@@ -1,12 +1,19 @@
 package com.morrowbone.mafiacards.app.application
 
 import androidx.multidex.MultiDexApplication
+import com.morrowbone.mafiacards.app.data.AppDatabase
 
 class MafiaApp : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
         instance = this
+        forceDatabaseInitialisation()
+    }
+
+    private fun forceDatabaseInitialisation() {
+        val database = AppDatabase.getInstance(applicationContext)
+        database.deckDao().getDecks()
     }
 
     companion object {
