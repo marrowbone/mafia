bc119493de2cd2c4a048a28b1a54eadbeb959eb6
Andrei Churyla <AndrewChuriloMinsk@gmail.com>
2019-08-23 00:55:00 +0300

    Add firebase-analytics
    Closes https://trello.com/c/d2AZnUU2/47-add-google-analytics

--- app/src/main/java/com/morrowbone/mafiacards/app/activity/MainActivity.kt
+++ app/src/main/java/com/morrowbone/mafiacards/app/activity/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.morrowbone.mafiacards.app.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
     }
 }
